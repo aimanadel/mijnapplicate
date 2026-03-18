ba5c733ee5e1a4a740384d1eade2f484d299694e
saeda adam hassan <saedams.ad@hva.nl>
2026-03-18 13:49:46 +0100

    Logo met vieknat teken toegevoegd

--- turtle_tekening.py
+++ turtle_tekening.py
@@ -1,26 +1,80 @@
-
 import turtle
 
-class Logo:
+class LogoANSW:
     def __init__(self):
         self.t = turtle.Turtle()
-        self.t.pensize(3)
+        self.t.speed(3)
+        self.t.pensize(4)
 
-    def teken_w(self):
+    def teken_vierkant(self, x, y, grootte):
         self.t.penup()
-        self.t.goto(250, 0)
+        self.t.goto(x, y)
         self.t.pendown()
+        self.t.color("black")
+        for _ in range(4):
+            self.t.forward(grootte)
+            self.t.right(90)
+        self.t.penup()
 
-        self.t.right(75)
-        self.t.forward(100)
-        self.t.left(150)
-        self.t.forward(100)
+    def teken_A(self):
+        self.t.color("red")
+        self.t.penup()
+        self.t.goto(10, 0)  # aangepaste positie
+        self.t.pendown()
+        self.t.left(75)
+        self.t.forward(60)   # kleiner
         self.t.right(150)
-        self.t.forward(100)
+        self.t.forward(60)   # kleiner
+        self.t.backward(30)
+        self.t.right(105)
+        self.t.forward(15)
+        self.t.setheading(0)
+
+    def teken_N(self):
+        self.t.color("green")
+        self.t.penup()
+        self.t.goto(80, 0)   # positie aangepast
+        self.t.pendown()
+        self.t.left(90)
+        self.t.forward(60)
+        self.t.right(150)
+        self.t.forward(70)
         self.t.left(150)
-        self.t.forward(100)
+        self.t.forward(60)
+        self.t.setheading(0)
 
-logo = Logo()
-logo.teken_w()
+    def teken_S(self):
+        self.t.color("blue")
+        self.t.penup()
+        self.t.goto(150, 30)  # positie aangepast
+        self.t.pendown()
+        self.t.right(90)
+        self.t.circle(20, 180)  # kleiner
+        self.t.circle(-20, 180)
+        self.t.setheading(0)
 
+    def teken_W(self):
+        self.t.color("purple")
+        self.t.penup()
+        self.t.goto(210, 0)   # positie aangepast
+        self.t.pendown()
+        self.t.right(75)
+        self.t.forward(60)
+        self.t.left(150)
+        self.t.forward(60)
+        self.t.right(150)
+        self.t.forward(60)
+        self.t.left(150)
+        self.t.forward(60)
+        self.t.setheading(0)
+
+# Maak object en teken het kleinere vierkant + logo
+logo = LogoANSW()
+logo.teken_vierkant(0, 100, 300)  # kleiner vierkant, past beter
+logo.teken_A()
+logo.teken_N()
+logo.teken_S()
+logo.teken_W()
+
+turtle.hideturtle()
 turtle.done()
