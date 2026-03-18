8f492d9113c85998776dfd8488f6e314747a9ea2
Aiman <aiman.al.qumairi@hva.nl>
2026-03-18 12:40:58 +0100

    Voeg lus toe voor vijf punten van de ster

--- app/Turtle_star.py
+++ app/Turtle_star.py
@@ -1,10 +1,17 @@
 import turtle
 
-ayman = turtle.Turtle()
-ayman.getscreen().bgcolor("#994444")
-ayman.color("yellow")
-ayman.speed(0)
+aiman = turtle.Turtle()
+aiman.getscreen().bgcolor("#994444")
+aiman.color("yellow")
+aiman.speed(0)
 
 def star(t, size):
     if size <= 10:
-     
+        return
+    for i in range(5):
+        t.forward(size)
+        star(t, size/2)
+        t.left(216)
+
+star(aiman, 100)
+turtle.done()
