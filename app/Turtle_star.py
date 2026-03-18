49150de91a07e3b331d9b06bdd5ff9f71413fcdb
Aiman <aiman.al.qumairi@hva.nl>
2026-03-18 12:38:56 +0100

    Definieer functie star

--- app/Turtle_star.py
+++ app/Turtle_star.py
@@ -5,3 +5,13 @@ ayman.getscreen().bgcolor("#994444")
 ayman.color("yellow")
 ayman.speed(0)
 
+def star(t, size):
+    if size <= 10:
+        return
+    for i in range(5):
+        t.forward(size)
+        star(t, size/2)
+        t.left(216)
+
+star(ayman, 100)
+turtle.done()
