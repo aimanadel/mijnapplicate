2e0ad3ad3904d39d859e3e87c2e61d415a9d5001
Aiman <aiman.al.qumairi@hva.nl>
2026-03-18 12:39:24 +0100

    Voeg stopvoorwaarde toe voor kleine sterren

--- app/Turtle_star.py
+++ app/Turtle_star.py
@@ -7,11 +7,4 @@ ayman.speed(0)
 
 def star(t, size):
     if size <= 10:
-        return
-    for i in range(5):
-        t.forward(size)
-        star(t, size/2)
-        t.left(216)
-
-star(ayman, 100)
-turtle.done()
+     
