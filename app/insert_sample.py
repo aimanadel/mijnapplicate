79ddc9d344d6c1b29cbb34483a3be995b8d6ea0a
saeda adam hassan <saedams.ad@hva.nl>
2026-04-17 16:55:58 +0200

    een data toegevoegd

--- app/insert_sample.py
+++ app/insert_sample.py
@@ -10,12 +10,12 @@ def insert_sample_data():
         "INSERT INTO leerling (naam, klas) VALUES ('Piet Peters', 'Klas 3B');",
         "INSERT INTO resultaat (leerling_id, onderwerp, score) VALUES (1, 'Rekenen', 75);",
         "INSERT INTO resultaat (leerling_id, onderwerp, score) VALUES (1, 'Taal', 82);",
-        "INSERT INTO resultaat (leerling_id, onderwerp, score) VALUES (1, 'Wetenschap', 68);",
+        "INSERT INTO resultaat (leerling_id, onderwerp, score) VALUES (1, 'biologie ', 68);",
         "INSERT INTO fout (leerling_id, categorie, subcategorie, aantal) VALUES (1, 'Rekenen', 'Optellen', 5);",
         "INSERT INTO fout (leerling_id, categorie, subcategorie, aantal) VALUES (1, 'Rekenen', 'Aftrekken', 3);",
         "INSERT INTO fout (leerling_id, categorie, subcategorie, aantal) VALUES (1, 'Taal', 'Spelling', 7);",
         "INSERT INTO fout (leerling_id, categorie, subcategorie, aantal) VALUES (1, 'Taal', 'Zinsbouw', 2);",
-        "INSERT INTO fout (leerling_id, categorie, subcategorie, aantal) VALUES (1, 'Wetenschap', 'Biologie', 4);",
+        "INSERT INTO fout (leerling_id, categorie, subcategorie, aantal) VALUES (1, 'biologie ', 'Cellen', 4);",
     ]
 
     for insert in inserts:
