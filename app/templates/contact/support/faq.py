797e4c34cd9564114acfcad4c913809ff5a84460
Nila <nila.naebkhill@hva.nl>
2026-04-19 21:45:42 +0200

    een class aangemaakt voor faq

---
+++ app/templates/contact/support/faq.py
@@ -0,0 +1,5 @@
+class FAQ:
+    """
+    Representeert een veelgestelde vraag (Frequently Asked Question).
+    Deze class wordt gebruikt om data uit de database te transformeren naar een object.
+    """
