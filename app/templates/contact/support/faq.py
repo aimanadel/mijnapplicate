ff1652ccd7a1b44e6f48d49860e9273bccdd54f9
Nila <nila.naebkhill@hva.nl>
2026-04-19 22:39:58 +0200

    nog een class aangemaakt

--- app/templates/contact/support/faq.py
+++ app/templates/contact/support/faq.py
@@ -23,3 +23,29 @@ def get_summary(self):
             # Snijd de tekst af en voeg puntjes toe voor een nette weergave
             return self.answer[:50] + "..."
     return self.answer
+
+class ContactMessage:
+    """
+    Representeert een door de gebruiker ingezonden contactbericht.
+    Bevat logica voor data-validatie alvorens opslag in de database.
+    """
+
+    def __init__(self, name, email, message_text):
+        """
+        Initialiseert een ContactMessage object.
+        :param name: Naam van de afzender (str).
+        :param email: E-mailadres van de afzender (str).
+        :param message_text: De inhoud van het bericht (str).
+        """
+        self.name = name
+        self.email = email
+        self.message_text = message_text
+
+    def is_valid(self):
+        """
+        Controleert of de invoer voldoet aan de minimale eisen.
+        :return: True als velden gevuld zijn en email een @ bevat, anders False.
+        """
+        if not self.name or not self.email or not self.message_text:
+            return False
+        return "@" in self.email
