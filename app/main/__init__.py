683cc13c5d2b1ab4198c2bf557257af5b18d0060
saeda adam hassan <saedams.ad@hva.nl>
2026-04-20 13:54:31 +0200

    classen in een bestand

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -6,15 +6,9 @@ bp = Blueprint("main", __name__)
 
 from app.main import routes
 
-# Laad oefenenopgaven routes
-route_file = os.path.join(os.path.dirname(__file__), "route.oefenenopgaven.py")
-spec = importlib.util.spec_from_file_location("app.main.route.oefenenopgaven", route_file)
-route_module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(route_module)
-
 # Laad foutenanalyse routes
 route_file = os.path.join(os.path.dirname(__file__), "route.foutenanalyse.py")
-spec = importlib.util.spec_from_file_location("app.main.route.foutenanalyse", route_file)
+spec = importlib.util.spec_from_file_location("app.main.foutenanalyse", route_file)
 route_module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(route_module)
 
